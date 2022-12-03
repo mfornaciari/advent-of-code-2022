b28efb985d2398e9ff86bbea989b649fa736e9b0
Marco Fornaciari <mfornaciari@gmail.com>
2022-12-03 19:49:50 -0300

    refactor day 3 again

--- day-3/rucksack_reorganization.rb
+++ day-3/rucksack_reorganization.rb
@@ -4,25 +4,18 @@ rucksacks = File.read('input.txt').split
 
 def solve_first_puzzle(rucksacks)
   rucksacks.sum do |rucksack|
-    split_rucksack = split_rucksack(rucksack)
+    split_rucksack = rucksack.chars.each_slice(rucksack.size / 2).to_a
     found_item = (split_rucksack[0] & split_rucksack[1]).first
     calculate_value(found_item)
   end
 end
 
 def solve_second_puzzle(rucksacks)
-  groups = rucksacks.each_slice(3).to_a
-  groups.sum do |group|
+  rucksacks.each_slice(3).to_a.sum do |group|
     split_group = group.map(&:chars)
     badge = (split_group[0] & split_group[1] & split_group[2]).first
     calculate_value(badge)
   end
-end
-
-def split_rucksack(rucksack)
-  first_half = rucksack[0...rucksack.size / 2].chars
-  second_half = rucksack[rucksack.size / 2..rucksack.size].chars
-  [first_half, second_half]
 end
 
 def calculate_value(character)
