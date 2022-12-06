2ca53e5be67462346d7760b078c03d6ffa4a981a
Marco Fornaciari <mfornaciari@gmail.com>
2022-12-06 08:46:14 -0300

    solve day 5 part 2

--- day-5/supply_stacks.rb
+++ day-5/supply_stacks.rb
@@ -1,33 +1,45 @@
 # frozen_string_literal: true
 
-input = File.read('input.txt').split("\n\n")
-
-crates = input.first.split("\n")[0...-1].map do |line|
-  split_line = line.chars.each_slice(4).map do |element|
-    element.join.strip.gsub(/\W/, '')
-  end
-  split_line + [''] * (9 - split_line.length)
-end
-
-columns = crates.transpose.map { |column| column.reject(&:empty?) }
-
+input = File.read('input.txt').split("\n\n").freeze
 instructions = input.last.split("\n").map do |instruction|
   instruction.split.map(&:to_i).filter(&:nonzero?)
 end
 
-def move_crates(instructions, columns)
+def crates(input)
+  input.first.split("\n")[0...-1].map do |line|
+    split_line = line.chars.each_slice(4).map do |element|
+      element.join.strip.gsub(/\W/, '')
+    end
+    split_line + [''] * (9 - split_line.length)
+  end
+end
+
+def columns(crates)
+  crates.transpose.map { |column| column.reject(&:empty?) }
+end
+
+def move_crates(instructions, columns, crane: 9000)
   resulting_columns = columns
   instructions.each do |instruction|
     number_of_boxes, column_from, column_to = instruction
     moved_crates = resulting_columns[column_from - 1].shift(number_of_boxes)
+    moved_crates = moved_crates.reverse if crane == 9001
     moved_crates.each { |crate| resulting_columns[column_to - 1].unshift(crate) }
   end
   resulting_columns
 end
 
-def solve_first_puzzle(instructions, columns)
+def solve_first_puzzle(input, instructions)
+  columns = columns(crates(input))
   final_columns = move_crates(instructions, columns)
   final_columns.map(&:first).join
 end
 
-solve_first_puzzle(instructions, columns)
+def solve_second_puzzle(input, instructions)
+  columns = columns(crates(input))
+  final_columns = move_crates(instructions, columns, crane: 9001)
+  final_columns.map(&:first).join
+end
+
+solve_first_puzzle(input, instructions)
+solve_second_puzzle(input, instructions)
