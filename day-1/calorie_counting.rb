82ebf3fe46fec10004f527ec2f90ea5a315e109f
Marco Fornaciari <mfornaciari@gmail.com>
2022-12-08 21:05:38 -0300

    refactor day 1

--- day-1/calorie_counting.rb
+++ day-1/calorie_counting.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-elves = File.read('input.txt').split("\n\n")
+ELVES = File.read('input.txt').split("\n\n").freeze
+TOTAL_CALORIES = ELVES.map { |elf_calories| elf_calories.split.map(&:to_i).sum }.freeze
 
-total_calories = elves.map { |elf_calories| elf_calories.split.map(&:to_i).sum }
+def solve_first_puzzle
+  TOTAL_CALORIES.max
+end
 
-total_calories.max(3).sum
+def solve_second_puzzle
+  TOTAL_CALORIES.max(3).sum
+end
+
+solve_first_puzzle
+solve_second_puzzle
