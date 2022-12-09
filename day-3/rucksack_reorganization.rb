be34e7aa28c14f42f63287d8a6a1219e137faec2
Marco Fornaciari <mfornaciari@gmail.com>
2022-12-08 21:19:40 -0300

    refactor day 3

--- day-3/rucksack_reorganization.rb
+++ day-3/rucksack_reorganization.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-rucksacks = File.read('input.txt').split
-
-def solve_first_puzzle(rucksacks)
-  rucksacks.sum do |rucksack|
-    split_rucksack = rucksack.chars.each_slice(rucksack.size / 2).to_a
-    found_item = (split_rucksack[0] & split_rucksack[1]).first
-    calculate_value(found_item)
-  end
-end
-
-def solve_second_puzzle(rucksacks)
-  rucksacks.each_slice(3).to_a.sum do |group|
-    split_group = group.map(&:chars)
-    badge = (split_group[0] & split_group[1] & split_group[2]).first
-    calculate_value(badge)
-  end
-end
+RUCKSACKS = File.read('input.txt').split.freeze
 
 def calculate_value(character)
   character == character.downcase ? character.ord - 96 : character.ord - 38
 end
 
-solve_first_puzzle(rucksacks)
-solve_second_puzzle(rucksacks)
+def solve_first_puzzle
+  RUCKSACKS.sum do |rucksack|
+    first_half, second_half = rucksack.chars.each_slice(rucksack.size / 2).to_a
+    found_item = (first_half & second_half).first
+    calculate_value(found_item)
+  end
+end
+
+def solve_second_puzzle
+  RUCKSACKS.each_slice(3).to_a.sum do |group|
+    first_elf, second_elf, third_elf = group.map(&:chars)
+    badge = (first_elf & second_elf & third_elf).first
+    calculate_value(badge)
+  end
+end
+
+solve_first_puzzle
+solve_second_puzzle
